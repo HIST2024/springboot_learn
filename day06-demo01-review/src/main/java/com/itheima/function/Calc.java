c19a7bfc107cb5ddf72f1e3ff22fc5156ded5e69
codermao <1301878596@qq.com>
2023-03-06 23:14:12 +0800

    Initial commit

---
+++ day06-demo01-review/src/main/java/com/itheima/function/Calc.java
@@ -0,0 +1,5 @@
+package com.itheima.function;/**
+ * @author codermao
+ * @date 2023/3/6 23:10
+ */public class Calc {
+}
---
+++ day06-demo01-review/src/test/java/com/itheima/test/CalcTest.java
@@ -0,0 +1,5 @@
+package com.itheima.test;/**
+ * @author codermao
+ * @date 2023/3/6 23:11
+ */public class CalcTest {
+}
